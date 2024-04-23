f05630b7a188088d7b6671fdade518fe27262b25
ArthurDiogenes <128557042+ArthurDiogenes@users.noreply.github.com>
2024-04-23 17:49:50 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+function sortear() {
+    let quantidade = document.getElementById('quantidade').value;
+    let de = document.getElementById('de').value;
+    let ate = document.getElementById('ate').valuel;
+
+    alert(`Quantidade: ${quantidade}`);
+    alert(`Do numero: ${de}`);
+    alert(`Ate o numero: ${ate}`);
+}
+
+function reiniciar() {
+    
+}
